a64a8c5ee4b62555c80e5b14e0e1a31f8890a7ae
Khánh Nguyễn Quốc <nguyenquockhanh10a10@gmail.com>
2021-11-19 13:55:18 +0700

    u2 them chuc nang tich

--- source.cpp
+++ source.cpp
@@ -37,6 +37,8 @@ int main()
 	cout << "Chon phep toan cong:  ";
 	cin >> tong(a, b);
 >>>>>>> bf8d1cd75aa281bbc7033c38a236bacd041cbce8
+	//User2 
+	//Chuc nang nhan///////////
 	cout << "Chon phep toan cong:  ";
 	cin >> nhan(a, b);
 	cout << "Chon phep toan cong:  ";
